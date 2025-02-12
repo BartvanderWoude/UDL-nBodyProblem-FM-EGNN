6e4e8e794e4966067835fe144d59d127e2739ecb
Tim E. <tim.eckhardt@hotmail.com>
2025-02-12 16:58:06 +0100

    added inference using a unified solver, disabled extra linear layers between egnn layers.

--- nbody_fm/inference.py
+++ nbody_fm/inference.py
@@ -1,3 +1,4 @@
+import torch
 from nbody_fm.model import CoorsWrappedModel, VelWrappedModel
 
 from tqdm import tqdm
@@ -28,6 +29,34 @@ def infer(vf, dataset, inference_method, inference_steps, output_file, step_size
 
         x_0 = out_x
         vel_0 = out_vel
+
+        output = f"{x_0[0, 0, 0].item()},{x_0[0, 0, 1].item()},"
+        output += f"{x_0[0, 1, 0].item()},{x_0[0, 1, 1].item()},"
+        output += f"{x_0[0, 2, 0].item()},{x_0[0, 2, 1].item()}\n"
+        inferred.write(output)
+
+        idx += 1
+
+
+def infer_unified(vf, dataset, inference_method, inference_steps, output_file, step_size=0.01, look_ahead=1):
+    if inference_method in ["dopri8", "dopri5", "bosh3", "fehlberg2", "adaptive_heun"]:
+        step_size = None
+
+    solver = ODESolver(velocity_model=vf)
+
+    inferred = open(output_file, "w")
+    idx = 0
+
+    for i in tqdm(range(inference_steps)):
+        if i % look_ahead == 0:
+            x_0, vel_0, _, _ = dataset[i]
+            x_0 = x_0.unsqueeze(0)
+            vel_0 = vel_0.unsqueeze(0)
+
+        out = solver.sample(x_init=torch.vstack((x_0, vel_0)), step_size=step_size, method=inference_method)
+
+        x_0 = out[0]
+        vel_0 = out[1]
 
         output = f"{x_0[0, 0, 0].item()},{x_0[0, 0, 1].item()},"
         output += f"{x_0[0, 1, 0].item()},{x_0[0, 1, 1].item()},"
