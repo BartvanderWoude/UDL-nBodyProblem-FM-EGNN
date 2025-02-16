7e833420adee0dddd48c27ab79908e069c898465
BartvanderWoude <b.r.van.der.woude@student.rug.nl>
2025-02-16 23:12:44 +0100

    Cleaned up code and updated README.

--- experiments/gridsearch.sh
+++ experiments/gridsearch.sh
@@ -8,36 +8,24 @@
 # beta: 0.5, 0.7
 
 # Main train
-python train.py --nepochs 35 --batch_size 32 --lr 0.0001 --feature_dim 4 --beta 0.5
+python train.py --nepochs 70 --batch_size 32 --lr 0.0001 --feature_dim 4 --beta 0.5
 
-# UDL1
-# python train.py --nepochs 12 --batch_size 32 --lr 0.001 --feature_dim 4 --beta 0.5
-# python train.py --nepochs 12 --batch_size 32 --lr 0.001 --feature_dim 4 --beta 0.7
-# python train.py --nepochs 12 --batch_size 32 --lr 0.0001 --feature_dim 4 --beta 0.5
-# python train.py --nepochs 12 --batch_size 32 --lr 0.0001 --feature_dim 4 --beta 0.7
+python train.py --nepochs 12 --batch_size 32 --lr 0.001 --feature_dim 4 --beta 0.5
+python train.py --nepochs 12 --batch_size 32 --lr 0.001 --feature_dim 4 --beta 0.7
+python train.py --nepochs 12 --batch_size 32 --lr 0.0001 --feature_dim 4 --beta 0.5
+python train.py --nepochs 12 --batch_size 32 --lr 0.0001 --feature_dim 4 --beta 0.7
 
-# UDL2
-# python train.py --nepochs 12 --batch_size 64 --lr 0.001 --feature_dim 4 --beta 0.5
-# python train.py --nepochs 12 --batch_size 64 --lr 0.001 --feature_dim 4 --beta 0.7
-# python train.py --nepochs 12 --batch_size 64 --lr 0.0001 --feature_dim 4 --beta 0.5
-# python train.py --nepochs 12 --batch_size 64 --lr 0.0001 --feature_dim 4 --beta 0.7
+python train.py --nepochs 12 --batch_size 64 --lr 0.001 --feature_dim 4 --beta 0.5
+python train.py --nepochs 12 --batch_size 64 --lr 0.001 --feature_dim 4 --beta 0.7
+python train.py --nepochs 12 --batch_size 64 --lr 0.0001 --feature_dim 4 --beta 0.5
+python train.py --nepochs 12 --batch_size 64 --lr 0.0001 --feature_dim 4 --beta 0.7
 
-# UDL3
-# python train.py --nepochs 20 --batch_size 32 --lr 0.001 --feature_dim 4 --beta 0.5
-# python train.py --nepochs 20 --batch_size 32 --lr 0.001 --feature_dim 4 --beta 0.7
-# python train.py --nepochs 20 --batch_size 32 --lr 0.0001 --feature_dim 4 --beta 0.5
-# python train.py --nepochs 20 --batch_size 32 --lr 0.0001 --feature_dim 4 --beta 0.7
+python train.py --nepochs 20 --batch_size 32 --lr 0.001 --feature_dim 4 --beta 0.5
+python train.py --nepochs 20 --batch_size 32 --lr 0.001 --feature_dim 4 --beta 0.7
+python train.py --nepochs 20 --batch_size 32 --lr 0.0001 --feature_dim 4 --beta 0.5
+python train.py --nepochs 20 --batch_size 32 --lr 0.0001 --feature_dim 4 --beta 0.7
 
-# UDL4
-# python train.py --nepochs 20 --batch_size 64 --lr 0.001 --feature_dim 4 --beta 0.5
-# python train.py --nepochs 20 --batch_size 64 --lr 0.001 --feature_dim 4 --beta 0.7
-# python train.py --nepochs 20 --batch_size 64 --lr 0.0001 --feature_dim 4 --beta 0.5
-# python train.py --nepochs 20 --batch_size 64 --lr 0.0001 --feature_dim 4 --beta 0.7
-
-touch models/keep.txt
-touch losses/keep.txt
-touch infer/keep.txt
-
-cp models/* /data/UDL/models/
-cp losses/* /data/UDL/losses/
-cp infer/* /data/UDL/infer/
+python train.py --nepochs 20 --batch_size 64 --lr 0.001 --feature_dim 4 --beta 0.5
+python train.py --nepochs 20 --batch_size 64 --lr 0.001 --feature_dim 4 --beta 0.7
+python train.py --nepochs 20 --batch_size 64 --lr 0.0001 --feature_dim 4 --beta 0.5
+python train.py --nepochs 20 --batch_size 64 --lr 0.0001 --feature_dim 4 --beta 0.7
